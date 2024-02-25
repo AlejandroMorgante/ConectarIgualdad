d101302d64b1a8a1d3c600825c5d9c37dde262e6
amorgante <alejandro.morgante@uala.com.ar>
2024-02-24 22:00:18 -0300

    add distribuidor oficial

---
+++ stored_procedures/distribuidor_oficial/distribuidor_oficial.sql
@@ -0,0 +1,28 @@
+-- Crear un nuevo distribuidor oficial
+CREATE PROCEDURE distribuidor_oficial_create
+    @nombre VARCHAR(50)
+AS
+BEGIN
+    INSERT INTO distribuidor_oficial (nombre)
+    VALUES (@nombre)
+END;
+
+-- Modificar un distribuidor oficial existente
+CREATE PROCEDURE distribuidor_oficial_update
+    @id_distribuidor_oficial INT,
+    @nombre VARCHAR(50)
+AS
+BEGIN
+    UPDATE distribuidor_oficial
+    SET nombre = @nombre
+    WHERE id_distribuidor_oficial = @id_distribuidor_oficial
+END;
+
+-- Eliminar un distribuidor oficial existente
+CREATE PROCEDURE distribuidor_oficial_delete
+    @id_distribuidor_oficial INT
+AS
+BEGIN
+    DELETE FROM distribuidor_oficial
+    WHERE id_distribuidor_oficial = @id_distribuidor_oficial
+END;
