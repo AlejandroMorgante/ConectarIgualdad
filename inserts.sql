50dbc28b38c0284bef083b978f3e0977f6417ef1
hugojos <30799094+hugojos@users.noreply.github.com>
2024-02-27 19:04:27 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -166,6 +166,29 @@ EXEC disco_rigido_create 'Maxtor', 500
 EXEC disco_rigido_create 'Patriot', 1000
 EXEC disco_rigido_create 'PNY', 2000
 
+EXEC docente_create 11111101, 'Alan', 'Turing', 'Enigma Street', '1912-06-23', 1
+EXEC docente_create 11111102, 'Grace', 'Hopper', 'Binary Avenue', '1906-12-09', 2
+EXEC docente_create 11111103, 'Tim', 'Berners-Lee', 'WWW Road', '1955-06-08', 3
+EXEC docente_create 11111104, 'Ada', 'Lovelace', 'Analytical Engine Boulevard', '1815-12-10', 4
+EXEC docente_create 11111105, 'Nikola', 'Tesla', 'Alternating Current Lane', '1856-07-10', 5
+EXEC docente_create 11111106, 'Albert', 'Einstein', 'Relativity Street', '1879-03-14', 6
+EXEC docente_create 11111107, 'Steve', 'Wozniak', 'Apple Lane', '1950-08-11', 7
+EXEC docente_create 11111108, 'Linus', 'Torvalds', 'Linux Avenue', '1969-12-28', 8
+EXEC docente_create 11111109, 'Alan', 'Kay', 'Smalltalk Lane', '1940-05-17', 9
+EXEC docente_create 11111110, 'John', 'von Neumann', 'Computer Science Boulevard', '1903-12-28', 10
+EXEC docente_create 11111111, 'Richard', 'Stallman', 'Free Software Road', '1953-03-16', 11
+EXEC docente_create 11111112, 'Vint', 'Cerf', 'Internet Avenue', '1943-06-23', 12
+EXEC docente_create 11111113, 'Bjarne', 'Stroustrup', 'C++ Street', '1950-12-30', 13
+EXEC docente_create 11111114, 'Donald', 'Knuth', 'Algorithm Alley', '1938-01-10', 14
+EXEC docente_create 11111115, 'Alan', 'Cox', 'Kernel Road', '1968-07-22', 15
+EXEC docente_create 11111116, 'Guido', 'van Rossum', 'Python Path', '1956-01-31', 16
+EXEC docente_create 11111117, 'Margaret', 'Hamilton', 'Software Engineering Boulevard', '1936-08-17', 17
+EXEC docente_create 11111118, 'Dennis', 'Ritchie', 'UNIX Lane', '1941-09-09', 18
+EXEC docente_create 11111119, 'James', 'Gosling', 'Java Avenue', '1955-05-19', 19
+EXEC docente_create 11111120, 'Andrew', 'Tanenbaum', 'Networking Street', '1944-03-16', 20
+
+
+
 EXEC equipo_create 8, 1, 1
 EXEC equipo_create 16, 2, 2
 EXEC equipo_create 8, 3, 3
@@ -269,6 +292,7 @@ EXEC taller_x_recurso_create 18, 9
 EXEC taller_x_recurso_create 19, 10
 EXEC taller_x_recurso_create 3, 10
 
+
 EXEC servidor_create '192.168.1.10', 'ar-west-a', 1, 1, 1
 EXEC servidor_create '192.168.1.11', 'ar-west-b', 2, 2, 1
 EXEC servidor_create '192.168.1.12', 'ar-west-c', 3, 3, 1
@@ -290,27 +314,6 @@ EXEC servidor_create '192.168.1.27', 'ar-central-h', 18, 18, 1
 EXEC servidor_create '192.168.1.28', 'ar-central-i', 19, 19, 1
 EXEC servidor_create '192.168.1.29', 'ar-central-j', 20, 20, 1
 
-
-EXEC docente_create 11111101, 'Alan', 'Turing', 'Enigma Street', '1912-06-23', 1
-EXEC docente_create 11111102, 'Grace', 'Hopper', 'Binary Avenue', '1906-12-09', 2
-EXEC docente_create 11111103, 'Tim', 'Berners-Lee', 'WWW Road', '1955-06-08', 3
-EXEC docente_create 11111104, 'Ada', 'Lovelace', 'Analytical Engine Boulevard', '1815-12-10', 4
-EXEC docente_create 11111105, 'Nikola', 'Tesla', 'Alternating Current Lane', '1856-07-10', 5
-EXEC docente_create 11111106, 'Albert', 'Einstein', 'Relativity Street', '1879-03-14', 6
-EXEC docente_create 11111107, 'Steve', 'Wozniak', 'Apple Lane', '1950-08-11', 7
-EXEC docente_create 11111108, 'Linus', 'Torvalds', 'Linux Avenue', '1969-12-28', 8
-EXEC docente_create 11111109, 'Alan', 'Kay', 'Smalltalk Lane', '1940-05-17', 9
-EXEC docente_create 11111110, 'John', 'von Neumann', 'Computer Science Boulevard', '1903-12-28', 10
-EXEC docente_create 11111111, 'Richard', 'Stallman', 'Free Software Road', '1953-03-16', 11
-EXEC docente_create 11111112, 'Vint', 'Cerf', 'Internet Avenue', '1943-06-23', 12
-EXEC docente_create 11111113, 'Bjarne', 'Stroustrup', 'C++ Street', '1950-12-30', 13
-EXEC docente_create 11111114, 'Donald', 'Knuth', 'Algorithm Alley', '1938-01-10', 14
-EXEC docente_create 11111115, 'Alan', 'Cox', 'Kernel Road', '1968-07-22', 15
-EXEC docente_create 11111116, 'Guido', 'van Rossum', 'Python Path', '1956-01-31', 16
-EXEC docente_create 11111117, 'Margaret', 'Hamilton', 'Software Engineering Boulevard', '1936-08-17', 17
-EXEC docente_create 11111118, 'Dennis', 'Ritchie', 'UNIX Lane', '1941-09-09', 18
-EXEC docente_create 11111119, 'James', 'Gosling', 'Java Avenue', '1955-05-19', 19
-EXEC docente_create 11111120, 'Andrew', 'Tanenbaum', 'Networking Street', '1944-03-16', 20
 
 EXEC materia_create 'Introducción a la Programación'
 EXEC materia_create 'Algoritmos y Estructuras de Datos'
